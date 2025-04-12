d9d836d01f70e09b09f6d4bbddcc6ac3ce0b0e7f
Jane Billa <janebilla0710@gmail.com.com>
2025-04-12 17:50:22 -0400

    commented functions

--- liquidity-pool/src/contracts/Exchange.sol
+++ liquidity-pool/src/contracts/Exchange.sol
@@ -5,20 +5,37 @@ pragma solidity ^0.8.28;
 
 // Define a contract named Exchange
 contract Exchange {
-   // Declare a public state variable to store the value
-   uint256 public value;
+    // Declare a public state variable to store the value
+    uint256 public value;
 
+    // Function to increment the value by a given amount
+    // @param _value The amount to increment the value by
+    function increment(uint256 _value) public {
+        value += _value; // Add the input value to the current value
+    }
 
-   // Function to increment the value by a given amount
-   // @param _value The amount to increment the value by
-   function increment(uint256 _value) public {
-       value += _value; // Add the input value to the current value
-   }
+    // Function to retrieve the current value
+    // @return The current value of the state variable
+    function getValue() public view returns (uint256) {
+        return value; // Return the stored value
+    }
 
-
-   // Function to retrieve the current value
-   // @return The current value of the state variable
-   function getValue() public view returns (uint256) {
-       return value; // Return the stored value
-   }
+    function addLiquidity() public {
+        //adds liquidity and receives tokens
+        /*
+        make sure the user approved to move the tokens
+        calculate how much needs to be minted based on LP size
+        mint back to user so they know how much they own
+        add to reserves
+        */
+    }
+    function removeLiquidity() public {
+        //removes and burns liquidity pool tokens
+        /*
+    check if user has enough to burn first, if not error check
+    get back the correct amount of tokens A and B and update the LP
+    
+    */
+    }
+    //swap functions with AMM formula
 }
