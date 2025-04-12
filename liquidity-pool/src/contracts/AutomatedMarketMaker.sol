40464b05798332317b339dc25c105d101862a8f7
Tyler Nguyen <nguyenhtyler@gmail.com>
2025-04-12 18:09:46 -0400

    Start currency update logic

--- liquidity-pool/src/contracts/AutomatedMarketMaker.sol
+++ liquidity-pool/src/contracts/AutomatedMarketMaker.sol
@@ -7,18 +7,27 @@ pragma solidity ^0.8.28;
 contract AutomatedMoneyMaker {
    // Declare a public state variable to store the value
    uint256 public value;
+   int256 private constant K = 50;
+   int256 public currencyBalance1;
+   int256 public currencyBalance2;
 
 
    // Function to increment the value by a given amount
-   // @param _value The amount to increment the value by
-   function increment(uint256 _value) public {
-       value += _value; // Add the input value to the current value
-   }
+    function currency1UpdateBalance(int256 curr2Diff) public {
+        require(curr2Diff <= currencyBalance2);
+        int256 newCurrencyBalance1 = K / (currencyBalance2 - curr2Diff);
+    }
 
+    function currency2UpdateBalance(int256 curr1Diff) public {
+        require(curr1Diff <= currencyBalance1);
+        int256 newCurrencyBalance2 = K / (currencyBalance1 - curr1Diff);
+    }
 
-   // Function to retrieve the current value
-   // @return The current value of the state variable
-   function getValue() public view returns (uint256) {
-       return value; // Return the stored value
-   }
+    function currencyUpdateBase(int256 x, int256 y) private {
+    }
+
+    function currencyUpdateBalances(int256 curr1Diff, int256 curr2Diff) public {
+        currency1UpdateBalance(curr2Diff);
+        currency2UpdateBalance(curr1Diff);
+    }
 }
