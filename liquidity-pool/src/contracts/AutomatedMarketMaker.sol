20ded3977c1e9663a71fcc64a1190f2847e0227f
Tyler Nguyen <nguyenhtyler@gmail.com>
2025-04-13 16:02:09 -0400

    Add decimal support

--- liquidity-pool/src/contracts/AutomatedMarketMaker.sol
+++ liquidity-pool/src/contracts/AutomatedMarketMaker.sol
@@ -5,12 +5,23 @@ pragma solidity ^0.8.28;
 
 // Define a contract named AutomatedMarketMaker
 contract AutomatedMoneyMaker {
-    // Function to return the amount of output currency in pool
-    // @param curr1Balance Amount of currency 1 in pool
+    int256 FEE = 200;
+    int256 WAD = 1e18;
+    // Function to return the amount of output token in pool
+    // @param curr1Balance Token 1 amount in pool
     // @param curr1Change Integer amount of tokens that the user added/removed from the pool
     // @param k AMM constant
-    // @return Amount of currency 2 in pool
-    function currencyUpdate(int256 curr1Balance, int256 curr1Change, int256 k) public pure returns (int256) {
-        return k / (curr1Balance + curr1Change);
+    // @return Token 2 amount obtained by buyer
+    function updatePoolBalance(int256 token1Balance, int256 token1Change, int256 k) public view returns (int256) {
+        int256 newToken1Balance = (token1Balance + token1Change) * WAD;
+        int256 totalToken2Output = k / newToken1Balance;
+        int256 fee = (totalToken2Output / WAD) * FEE;
+        return (totalToken2Output - fee) / WAD;
+
     }
 }
+
+// k should be initialized as init x * init y
+
+// 1.5 million
+// 1 million
