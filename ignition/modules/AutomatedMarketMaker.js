f74bf9b7719e2906add650b44f9b984e7cd44fb5
Tyler Nguyen <nguyenhtyler@gmail.com>
2025-04-13 20:11:56 -0400

    Delete hardhat initialized in wrong dir

--- ignition/modules/AutomatedMarketMaker.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-
-module.exports = buildModule("AutomatedMarketMakerModule", (m) => {
- const amm = m.contract("AutomatedMarketMaker", []);
-  return { amm };
-});
